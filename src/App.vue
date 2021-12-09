3b0f1e2a08979338c1c815399120d566cd6addf7
leonaidass <leo22esquivel@gmail.com>
2021-12-09 20:57:56 -0300

    comsumoapi

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="app">
+   <v-app-bar app color="green">
+      <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/admin">Administrador</router-link>
+    </div>
+
+   </v-app-bar>
+     <v-main>
+       <router-view/>
+     </v-main>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #e1e6ec;
+
+    &.router-link-exact-active {
+      color: #fcfcfc;
+    }
+  }
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="home">
+    <!-- <v-btn @click="obtenerproductos()" class="primary mt-2" >Obtener Datos</v-btn> -->
+    <v-container>
+       <v-row>
+         <v-col cols="4" v-for="producto in productos" :key="producto.id">
+           <v-card>
+             <v-card-title>
+               {{producto.nombre}}
+             </v-card-title>
+             <v-card-text>
+               <img :src=producto.imagen alt="" style="width:300px">
+               <p>Precio: {{producto.precio}}</p>
+               <p>Cantidad: {{producto.cantidad}}</p>
+               
+
+             </v-card-text>
+           </v-card>
+         </v-col>
+       </v-row>
+    </v-container>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      productos:[]
+    }
+  },
+  methods:{
+    obtenerproductos(){
+        fetch("https://61afe8ff3e2aba0017c4959a.mockapi.io/productos")
+        .then(res => res.json())
+        .then(datos =>{
+          this.productos=datos
+        })
+    }
+  },
+  mounted(){
+    this.obtenerproductos()
+  }
+ 
+  
+}
+</script>
